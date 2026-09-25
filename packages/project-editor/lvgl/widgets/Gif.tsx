e0eff19090f8e8906f4b934db878336e6d710772
IWILLTBEST <609807491@qq.com>
2026-09-25 14:56:48 +0800

    LVGL GIF: fix palette icon
    The old path combined a camera body, a lens circle and two plus
    subpaths that overlapped the lens and spilled outside the rounded
    frame (odd rendering with evenodd fill). Replaced with the standard
    animated-image metaphor: rounded frame with a centered play triangle.

--- packages/project-editor/lvgl/widgets/Gif.tsx
+++ packages/project-editor/lvgl/widgets/Gif.tsx
@@ -163,9 +163,8 @@ export class LVGLGifWidget extends LVGLWidget {
         icon: (
             <svg viewBox="0 0 24 24">
                 <path
-                    d="M4 5h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2zm8 5a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm-5-1H5v2H3v2h2v2h2v-2h2v-2H7V9zm10 0h-2v2h-2v2h2v2h2v-2h2v-2h-2V9z"
+                    d="M4 5h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V7a2 2 0 0 1 2-2zm6 4v6l6-3-6-3z"
                     fill="currentcolor"
-                    fillRule="evenodd"
                 />
             </svg>
         ),
